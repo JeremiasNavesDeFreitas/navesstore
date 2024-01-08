d37f62fd83a0e5abe32ef3af51d7b0b26c673cab
aryus1 <marcelocoamae@gmail.com>
2024-01-08 16:37:04 -0300

    v.0.03 - Criação dos componentes e início do header

---
+++ navesstore/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import styles from './Header.module.scss';
+import Logo from '../../assets/homepage/Naves logo White 1.png';
+import Carrinho from '../../assets/homepage/Design sem nome 1.png';
+
+export default function Header(){
+    return(
+        <header>
+            <div className={styles.box}>
+                <img className={styles.box__logo} src={Logo} alt='imagem logo'/>
+                <ul className={styles.box__lista}>
+                    <li className={styles.box__lista}> PEÇAS </li>
+                    <li className={styles.box__lista}> MINIATURAS </li>
+                    <li className={styles.box__lista}> PROTESES </li>
+                </ul>
+                <img className={styles.box__carrinho} src={Carrinho} alt='imagem carrinho'/>
+            </div>
+        </header>
+    )
+}
---
+++ navesstore/src/pages/HomePage/index.tsx
@@ -0,0 +1,10 @@
+import Header from '../../components/Header';
+import styles from './HomePage.module.scss';
+
+export default function HomePage(){
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+}
