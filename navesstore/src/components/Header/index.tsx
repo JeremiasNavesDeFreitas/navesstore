404b531f5292b0279055896cfab41b1881c5ff95
aryus1 <marcelocoamae@gmail.com>
2024-01-09 15:05:30 -0300

    v0.04 - Criação de componentes e Header feito.

--- navesstore/src/components/Header/index.tsx
+++ navesstore/src/components/Header/index.tsx
@@ -9,7 +9,12 @@ export default function Header(){
                 <img className={styles.box__logo} src={Logo} alt='imagem logo'/>
                 <ul className={styles.box__lista}>
                     <li className={styles.box__lista}> PEÇAS </li>
-                    <li className={styles.box__lista}> MINIATURAS </li>
+                    <div className={styles.circulo__box}>
+                        <li className={styles.box__lista}> MINIATURAS </li>
+                        <div className={styles.circulo}></div>
+                        <div className={styles.circulo}></div>
+                        <div className={styles.circulo}></div>
+                    </div>
                     <li className={styles.box__lista}> PROTESES </li>
                 </ul>
                 <img className={styles.box__carrinho} src={Carrinho} alt='imagem carrinho'/>
